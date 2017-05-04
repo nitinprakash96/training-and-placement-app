ffb1707bf04c8f971c6274072857305d8b7869dd
Nitin Prakash <prakash.nitin63@gmail.com>
2017-05-04 21:04:52 +0530

    jobs

---
+++ src/alumni/Job.java
@@ -0,0 +1,26 @@
+package alumni;
+
+public class Job extends alumni{
+	private String post;
+	private String Company;
+	private int Apackage;
+	
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+	public String getCompany() {
+		return Company;
+	}
+	public void setCompany(String Company) {
+		this.Company = Company;
+	}
+	public int getPackage() {
+		return Apackage;
+	}
+	public void setPackage(int Package) {
+		this.Apackage = Package;
+	}
+}
