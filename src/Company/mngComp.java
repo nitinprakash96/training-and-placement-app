7849eb6266addbc3ba61662d06b0fe83dae06864
Nitin Prakash <prakash.nitin63@gmail.com>
2017-05-05 16:54:39 +0530

    make different options available for companies

---
+++ src/Company/mngComp.java
@@ -0,0 +1,15 @@
+package Company;
+
+import java.util.ArrayList;
+
+public class mngComp extends Company{
+private ArrayList<String> skill=new ArrayList<String>();
+	
+	public void setSkill(String skill){
+		this.skill.add(skill);
+	}
+	public ArrayList<String> getSkill(){
+		return skill;
+	}
+
+}
